02e6166a4994ae8523c20be425845325e5cbf42e
Elodie Ratovoherinjanahary <elodie.ratovoherinjanahary@epita.fr>
2022-03-30 05:56:35 +0200

    Fixing the client request

--- src/main/java/fr/lernejo/httphandler/CreateClient.java
+++ src/main/java/fr/lernejo/httphandler/CreateClient.java
@@ -20,7 +20,7 @@ public class CreateClient {
             .uri(URI.create(url + "/api/game/start"))
             .setHeader("Accept", "application/json")
             .setHeader("Content-Type", "application/json")
-            .POST(HttpRequest.BodyPublishers.ofString("{\\\"id\\\":\\\"\\\"\" + this.port + \"\\\", \\\"url\\\":\\\"http://localhost:\" + this.port + \"\\\", \"message\":\"hello\"}"))
+            .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"1\", \"url\":\"http://localhost:" + this.port + "\", \"message\":\"hello\"}"))
             .build();
 
         HttpClient client = HttpClient.newHttpClient();
